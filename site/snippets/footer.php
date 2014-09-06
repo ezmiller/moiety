bb3a2d3f17c4099407b93877fbd1f599c53d5d81
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2014-09-06 13:54:23 +0200

    fixed mistake in update to google analytics code

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -119,7 +119,7 @@
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-54421921-1', 'lanningsmith.com');
+		ga('create', 'UA-54421921-1', 'itsmoiety.com');
 		ga('send', 'pageview');
 	</script>
 
